a99b81c36230db53578fe7ad7a2ab32a0087f786
Ilia <vitttiv@yandex.ru>
2020-10-06 10:12:15 +0300

    test ok

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,10 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+    @Autowired
+    private Deth8 deth;
 }
--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,19 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Egg6 {
+
+    private Needle7 needdle;
+
+    public Egg6(Needle7 needdle) {  this.needdle = needdle; }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needdle.toString();
     }
+
+    @Autowired
+    public void setNeeddle(Needle7 needdle){ this.needdle = needdle; }
+
 }
